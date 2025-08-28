c7d1afd6c890ed0c44f6f77f4a046e23f9bde93d
DanielRosso <DanielRosso@users.noreply.github.com>
2025-08-28 07:52:12 +0000

    email und pw korrekt übergeben

--- yazio-export.js
+++ yazio-export.js
@@ -8,8 +8,8 @@ const n8nWebhookUrl = process.env.N8N_WEBHOOK_URL;
 
 const client = new Yazio({
   credential: {
-    username: "email",
-    password: "password"
+    username: email,
+    password: password
   }
 });
 
